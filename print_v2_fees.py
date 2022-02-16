0e0da4a633f9ba7698318e077d882c838b160187
emattia <40632488+emattia@users.noreply.github.com>
2022-02-16 05:37:17 -0800

    Update print_v2_fees.py

--- print_v2_fees.py
+++ print_v2_fees.py
@@ -1,17 +1,7 @@
 from brownie import Contract, network
+from constants import v2_pool_contracts
 network.connect('mainnet')
 provider = Contract('0x0000000022D53366457F9d5E68Ec105046FC4383')  # Curve Registry's Address Provider
 registry = Contract.from_explorer(provider.get_registry())
-v2_pool_contracts = {
-    "tricrypto2": "0xD51a44d3FaE010294C616388b506AcdA1bfAAE46", 
-    "tricrypto": "0x80466c64868E1ab14a1Ddf27A676C3fcBE638Fe5", 
-    "eurtusd": "0x9838eCcC42659FA8AA7daF2aD134b53984c9427b", 
-    "eursusd": "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 
-    "crveth": "0x8301AE4fc9c624d1D396cbDAa1ed877821D7C511", 
-    "cvxeth": "0xB576491F1E6e5E62f1d8F26062Ee822B40B0E0d4", 
-    "xautusd": "0xAdCFcf9894335dC340f6Cd182aFA45999F45Fc44", 
-    "spelleth": "0x98638FAcf9a3865cd033F36548713183f6996122", 
-    "teth": "0x752eBeb79963cf0732E9c0fec72a49FD1DEfAEAC", 
-}
 fees = [registry.get_fees(contract) for contract in v2_pool_contracts.values()]
 [print(f"{name}: {fee*1e-8} {admin_pct*1e-8}") for name, (fee, admin_pct) in zip(v2_pool_contracts.keys(), fees)]
